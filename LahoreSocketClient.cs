c345ffe8076b52b3c433cf228468bce8d92f64c6
Francis Starr <38301914+francisstarr@users.noreply.github.com>
2020-06-20 14:53:49 -0400

    commit

---
+++ LahoreSocketClient.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LahoreSocketAsync
+{
+    public class LahoreSocketClient
+    {
+        IPAddress mServerIPAddress;
+        int mServerPort;
+        TcpClient mClient;
+        public LahoreSocketClient() {
+            mServerPort = MyServer.mPort;
+            mClient = null;
+            mServerIPAddress = MyServer.mIp;
+        }
+        public IPAddress ServerIPAddress { get { return mServerIPAddress; } }
+        public int ServerPort { get { return mServerPort; } }
+        public bool SetServerIPAddress(string x) {
+            IPAddress ipaddr = null;
+            if (!IPAddress.TryParse(x, out ipaddr))
+            {
+                Console.WriteLine("Invalid server IP supplied");
+                return false;
+            }
+            this.mServerIPAddress = ipaddr;
+            return true;
+        }
+        public bool SetPortNumber(string x) {
+            int portNumber = 0;
+            if (!int.TryParse(x.Trim(), out portNumber)) {
+                Console.WriteLine("Invalid port number supplied");
+                return false;
+            }
+            if (portNumber <1 || portNumber > 65535) {
+                Console.WriteLine("Port Number must be between 1 and 65535");
+                return false;
+            }
+            this.mServerPort = portNumber;
+            return true;
+        }
+
+        public void CloseAndDisconnect()
+        {
+            if (mClient != null && mClient.Connected)
+            {
+                mClient.Close();
+            }
+        }
+
+        public async Task SendToServer(string strInputUser)
+        {
+            if (string.IsNullOrEmpty(strInputUser.Trim())) {
+                Console.WriteLine("Empty string supplied. Try again");
+                return;
+            }
+            if (mClient != null && mClient.Connected) {
+                StreamWriter clientStreamWriter = new StreamWriter(mClient.GetStream());
+                clientStreamWriter.AutoFlush = true;
+                await clientStreamWriter.WriteAsync(strInputUser);
+                Console.WriteLine("Data sent...");
+            }
+        }
+
+        public async Task ConnectToServer() {
+            if (this.mClient == null) {
+                mClient = new TcpClient();
+            }
+            try {
+                await mClient.ConnectAsync(mServerIPAddress, mServerPort);
+                Console.WriteLine(string.Format("Connected to server IP/Port: {0} / {1}", mServerIPAddress, mServerPort));
+                ReadDataAsync(mClient);
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
+        }
+
+        public async Task ReadDataAsync(TcpClient mClient)
+        {
+            try {
+                StreamReader clientStreamReader = new StreamReader(mClient.GetStream());
+                char[] buff = new char[64];
+                int readByteCount = 0;
+                while (true) {
+                    readByteCount=await clientStreamReader.ReadAsync(buff, 0, buff.Length);
+                    if (readByteCount <= 0) {
+                        Console.WriteLine("Disconnected from server");
+                        mClient.Close();
+                        break;
+                    }
+                    Console.WriteLine(string.Format("Received bytes: {0} - Message: {1}", readByteCount, new string(buff)));
+                    Array.Clear(buff, 0, buff.Length);
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
+        }
+    }
+}
---
+++ MyServer.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+
+
+namespace LahoreSocketAsync
+{
+    public class MyServer
+    {
+
+
+        public static int mPort = 23000;
+        public static string ipAddr = "192.168.0.2";
+        public static IPAddress mIp = IPAddress.Parse(ipAddr);
+        public static TcpListener mTCPListener;
+        public static List<TcpClient> mClients = new List<TcpClient>();
+        public static bool KeepRunning;
+        public static async void StartListeningForIncomingConnection()
+        {
+            System.Diagnostics.Debug.WriteLine(string.Format("IP Address: {0} - Port: {1}", MyServer.mIp.ToString(), MyServer.mPort));
+            mTCPListener = new TcpListener(mIp, mPort);
+            try
+            {
+
+                mTCPListener.Start();
+                KeepRunning = true;
+                while (KeepRunning)
+                {
+                    var returnedByAccept = await mTCPListener.AcceptTcpClientAsync();
+                    mClients.Add(returnedByAccept);
+                    System.Diagnostics.Debug.WriteLine(string.Format("Client connected success, count: {0} - {1}", mClients.Count, returnedByAccept.Client.RemoteEndPoint));
+                    TakeCareOfTcpClient(returnedByAccept);
+                }
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.ToString());
+            }
+        }
+
+        public static void StopServer()
+        {
+            try
+            {
+                if (mTCPListener != null)
+                {
+                    mTCPListener.Stop();
+                }
+                foreach (TcpClient c in mClients)
+                {
+                    c.Close();
+                }
+                System.Diagnostics.Debug.WriteLine("Server Closed");
+            }
+            catch (Exception e)
+            {
+                //System.Diagnostics.Debug.WriteLine(e.ToString());
+                System.Diagnostics.Debug.WriteLine("stop error");
+            }
+        }
+
+        public static async void TakeCareOfTcpClient(TcpClient x)
+        {
+            NetworkStream stream = null;
+            StreamReader reader = null;
+            try
+            {
+                stream = x.GetStream();
+                reader = new StreamReader(stream);
+                char[] buff = new char[64];
+                while (KeepRunning)
+                {
+                    int nRet = await reader.ReadAsync(buff, 0, buff.Length);
+                    System.Diagnostics.Debug.WriteLine(nRet);
+                    if (nRet == 0)
+                    {
+                        RemoveClient(x);
+                        System.Diagnostics.Debug.WriteLine("Socket disconnected");
+                        break;
+                    }
+                    string receivedText = new string(buff);
+                    System.Diagnostics.Debug.WriteLine(receivedText);
+                    Array.Clear(buff, 0, buff.Length);
+
+                }
+            }
+            catch (Exception e)
+            {
+                RemoveClient(x);
+                System.Diagnostics.Debug.WriteLine(e.ToString());
+            }
+        }
+
+        public static void RemoveClient(TcpClient x)
+        {
+            if (mClients.Contains(x))
+            {
+                mClients.Remove(x);
+                System.Diagnostics.Debug.WriteLine(String.Format("Client Removed, count: {0}", mClients.Count));
+            }
+        }
+        public static async void SendToAll(string m)
+        {
+
+            if (String.IsNullOrEmpty(m))
+            {
+                return;
+            }
+            try
+            {
+                byte[] buffMessage = Encoding.ASCII.GetBytes(m);
+                foreach (TcpClient c in mClients)
+                {
+                    c.GetStream().WriteAsync(buffMessage, 0, buffMessage.Length);
+                }
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.ToString());
+            }
+        }
+    }
+}
